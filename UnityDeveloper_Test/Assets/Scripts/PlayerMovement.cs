2e5c74e95d1745c2eb51c08d163b54ec203556ac
Debnitya Dutta Gupta <debnitya525@gmail.com>
2024-07-26 21:32:30 +0530

    Made Changes to 3rd Person Cam + Added Player Movement
    Player now rotates relatively to where the camera is looking + when having an obstacle behind now the Camera comes forward
    Player has basic movement with wasd keys and a jump function with space with additional drag settings

--- UnityDeveloper_Test/Assets/Scripts/PlayerMovement.cs
+++ UnityDeveloper_Test/Assets/Scripts/PlayerMovement.cs
@@ -4,15 +4,115 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [Header("Movement")]
+    public float moveSpeed;
+
+    public float groundDrag;
+
+    public float jumpForce;
+    public float jumpCooldown;
+    public float airMultiplier;
+    bool readyToJump = true;
+
+    [Header("Keybinds")]
+    public KeyCode jumpKey = KeyCode.Space;
+
+    [Header("Ground Check")]
+    public float playerHeight;
+    public LayerMask whatisGround;
+    bool grounded;
+
+    public Transform Player;
+
+    public float horizontalInput;
+    public float verticalInput;
+
+    Vector3 moveDirection;
+
+    Rigidbody rb;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        //ground check
+        Vector3 raycastOrigin = transform.position + Vector3.up * 0.1f;
+        grounded = Physics.Raycast(raycastOrigin, Vector3.down, playerHeight * 0.5f, whatisGround);
+        Debug.DrawRay(raycastOrigin, Vector3.down, Color.red);
+        MyInput();
+
+        if (grounded)
+        {
+            rb.drag = groundDrag;
+        }
+        else
+        {
+            rb.drag = 0f;
+        }
+
+        SpeedControl();
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    private void MyInput()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+
+        if(Input.GetKey(jumpKey) && readyToJump && grounded)
+        {
+            readyToJump = false;
+
+            Jump();
+
+            Invoke(nameof(ResetJump), jumpCooldown);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        moveDirection = Player.forward * verticalInput + Player.right * horizontalInput;
+
+        if(grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f, ForceMode.Force);
+            //Debug.Log("Grounded");
+        }
+        else if(!grounded)
+        {
+            rb.AddForce(moveDirection.normalized * moveSpeed * 10f * airMultiplier, ForceMode.Force);
+            //Debug.LogError("Warning Not Grounded");
+        }
+    }
+
+    private void SpeedControl()
+    {
+        Vector3 flatVel = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        if(flatVel.magnitude > moveSpeed)
+        {
+            Vector3 limitedVel = flatVel.normalized * moveSpeed;
+            rb.velocity = new Vector3(limitedVel.x, rb.velocity.y, limitedVel.z); ;
+        }
+    }
+
+    private void Jump()
+    {
+        rb.velocity = new Vector3(rb.velocity.x, 0f, rb.velocity.z);
+
+        rb.AddForce(transform.up * jumpForce, ForceMode.Impulse);
+    }
+
+    private void ResetJump()
+    {
+        readyToJump = true;
     }
 }
